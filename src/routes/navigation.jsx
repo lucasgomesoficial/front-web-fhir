7096462d08b68a490a61de36fb4f5efc4527a08c
Lucas Gomes <lucasgomesbr@gmail.com>
2023-10-03 20:12:59 -0300

    fix: routes
    melhor organizado s rotas

--- src/routes/navigation.jsx
+++ src/routes/navigation.jsx
@@ -1,50 +1,28 @@
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import { RequireAuth } from "../context/authProvider";
-import { Dashboard } from "../pages/DashboardPage";
-import { ErrorPage } from "../pages/ErrorPage";
-import { Login } from "../pages/LoginPage";
-import { About } from "../pages/AboutPage";
-import { Patient } from "@/pages/PatientPage";
+// import { RequireAuth } from "../context/authProvider";
+import { config } from "./config";
 
-function ProtectRoute({ Route }) {
-  return (
-    <RequireAuth>
-      <Route />
-    </RequireAuth>
-  );
-}
+// function ProtectRoute({ Route }) {
+//   return (
+//     <RequireAuth>
+//       <Route />
+//     </RequireAuth>
+//   );
+// }
 
 export function Navigation() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route
+      {config.map(({ path, component }) => {
+        return <Route key={path} path={path} element={component} />;
+      })}
+      {/* <Route
         path="/home"
         element={
           <ProtectRoute Route={Dashboard} />
         }
-      />
-      <Route
-        path="sobre"
-        element={
-          <Suspense fallback="Loading...">
-            <About />
-          </Suspense>
-        }
-      />
-      {/* <Route path="order-summary" element={<OrderSummary />} />
-      <Route path="products" element={<Products />}>
-        <Route index element={<FeaturedProducts />} />
-        <Route path="featured" element={<FeaturedProducts />} />
-        <Route path="new" element={<NewProducts />} />
-      </Route>
-      <Route path="users" element={<Users />}>
-        <Route path=":userId" element={<UserDetails />} />
-        <Route path="admin" element={<Admin />} />
-      </Route> */}
-      <Route path="*" element={<ErrorPage />} />
-      <Route path="/pacientes" element={<Patient />} />
+      /> */}
     </Routes>
   );
 }
